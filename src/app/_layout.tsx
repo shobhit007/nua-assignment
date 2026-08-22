852449bd1972a0cbec9bd3ab0d4d968bc6265bb0
shobhit saini <shobhitsaini709@gmail.com>
2026-08-22 12:07:26 +0530

    chore(project): clean up Expo starter code

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
+import { Colors } from "@/constants/theme";
+import { Stack } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: Colors.light.background },
+        animation: "ios_from_right",
+      }}
+    >
+      <Stack.Screen name="/list" />
+    </Stack>
   );
 }
---
+++ src/app/list.tsx
@@ -0,0 +1,14 @@
+import { ThemedText } from "@/components/ui/themed-text";
+import { StyleSheet, View } from "react-native";
+
+const List = () => {
+  return (
+    <View>
+      <ThemedText>Hello world!</ThemedText>
+    </View>
+  );
+};
+
+export default List;
+
+const styles = StyleSheet.create({});
