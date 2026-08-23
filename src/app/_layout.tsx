d8cee235960df92c9df8578345bce36f0b957ba7
shobhit saini <shobhitsaini709@gmail.com>
2026-08-23 14:05:17 +0530

    chore: initialize redux toolkit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,24 @@
 import { Colors } from "@/constants/theme";
+import { persistor, store } from "@/store";
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function TabLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: Colors.light.background },
-        animation: "ios_from_right",
-      }}
-    >
-      <Stack.Screen name="list" />
-      <Stack.Screen name="product/[id]" />
-    </Stack>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: Colors.light.background },
+            animation: "ios_from_right",
+          }}
+        >
+          <Stack.Screen name="list" />
+          <Stack.Screen name="product/[id]" />
+        </Stack>
+      </PersistGate>
+    </Provider>
   );
 }
---
+++ src/store/index.ts
@@ -0,0 +1,37 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+
+import { rootReducer } from "./rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: [] as string[],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
